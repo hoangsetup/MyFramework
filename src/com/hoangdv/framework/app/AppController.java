50f1844df94c7ce35b7309c73dd06ce2b132dbdc
Đinh Văn Hoàng <hoang.dv@outlook.com>
2014-10-31 10:48:03 +0700

    31.10.2014

---
+++ src/com/hoangdv/framework/app/AppController.java
@@ -0,0 +1,65 @@
+/*
+ * 
+ */
+package com.hoangdv.framework.app;
+
+import android.app.Application;
+import android.text.TextUtils;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.Volley;
+import com.hoangdv.framework.util.LruBitmapCache;
+
+public class AppController extends Application {
+	public static final String TAG = AppController.class.getSimpleName();
+	private RequestQueue mRequestQueue;
+	private ImageLoader mImageLoader;
+	public static AppController mInstance;
+
+	@SuppressWarnings("static-access")
+	@Override
+	public void onCreate() {
+		// TODO Auto-generated method stub
+		super.onCreate();
+		this.mInstance = this;
+	}
+
+	public static synchronized AppController getInstance() {
+		return mInstance;
+	}
+
+	public RequestQueue getRequestQueue() {
+		if (mRequestQueue == null) {
+			mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+		}
+		return this.mRequestQueue;
+	}
+
+	public ImageLoader getImageLoader() {
+		getRequestQueue();
+		if (mImageLoader == null) {
+			mImageLoader = new ImageLoader(this.mRequestQueue,
+					new LruBitmapCache());
+		}
+		return this.mImageLoader;
+	}
+
+	public <T> void addToRequesQueue(Request<T> req, String tag) {
+		req.setTag((TextUtils.isEmpty(tag) ? TAG : tag));
+		getRequestQueue().add(req);
+	}
+
+	public <T> void addToReqestQueue(Request<T> req) {
+		req.setTag(TAG);
+		getRequestQueue().add(req);
+	}
+
+	public void cancelPendingRequests(Object tag) {
+		if (mRequestQueue != null) {
+			mRequestQueue.cancelAll(tag);
+		}
+	}
+}
+
---
+++ src/com/hoangdv/framework/util/Utilities.java
@@ -0,0 +1,186 @@
+package com.hoangdv.framework.util;
+
+/*
+ * FileName: Utilities.java 
+ * Date: 30 Oct 2014 
+ * Desc:
+ */
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Random;
+
+import android.app.ActionBar.LayoutParams;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.WallpaperManager;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Environment;
+import android.util.Log;
+import android.view.Display;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.hoangdv.framework.R;
+
+/**
+ * @author Hoangdv
+ */
+public class Utilities {
+	private Context context;
+
+	public Utilities() {
+
+	}
+
+	/**
+	 * @param ctx
+	 */
+	public Utilities(Context ctx) {
+		context = ctx;
+	}
+
+	/**
+	 * @return
+	 */
+	@SuppressWarnings("deprecation")
+	public int getScreenWidth() {
+		int columnWidth = 1;
+		WindowManager windowManager = (WindowManager) context
+				.getSystemService(Context.WINDOW_SERVICE);
+		Display display = windowManager.getDefaultDisplay();
+		final Point point = new Point();
+		try {
+			display.getSize(point);
+		} catch (NoSuchMethodError ignore) {
+			point.x = display.getWidth();
+			point.y = display.getHeight();
+		}
+		columnWidth = point.x;
+		return columnWidth;
+	}
+
+	/**
+	 * @param bitmap
+	 * @return
+	 */
+	public boolean setWallpaperByBitmap(Bitmap bitmap) {
+		try {
+			WallpaperManager wallpaperManager = WallpaperManager
+					.getInstance(context);
+			wallpaperManager.setBitmap(bitmap);
+			return true;
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			return false;
+		}
+	}
+
+	/**
+	 * @param bitmap
+	 * @param fileName
+	 * @return
+	 */
+	public boolean saveBitmapToSDCard(Bitmap bitmap, String fileName) {
+		File myDir = new File(
+				Environment
+						.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+				"newDir");
+		myDir.mkdir();
+		if (fileName.isEmpty()) {
+			Random random = new Random();
+			int n = 10000;
+			n = random.nextInt(n);
+			fileName = "Image" + n + ".jpg";
+		}
+		File file = new File(myDir, fileName);
+		if (file.exists()) {
+			file.delete();
+		}
+		try {
+			FileOutputStream out = new FileOutputStream(file);
+			bitmap.compress(Bitmap.CompressFormat.JPEG, 90, out);
+			out.flush();
+			out.close();
+			return true;
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			return false;
+		}
+	}
+
+	/**
+	 * @return
+	 */
+	public boolean isConnectingToInternet() {
+		ConnectivityManager cManager = (ConnectivityManager) context
+				.getSystemService(Context.CONNECTIVITY_SERVICE);
+		if (cManager != null) {
+			NetworkInfo[] infos = cManager.getAllNetworkInfo();
+			if (infos != null) {
+				for (NetworkInfo i : infos) {
+					if (i.getState() == NetworkInfo.State.CONNECTED) {
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * @param title
+	 * @param message
+	 * @param status
+	 *            - Trạng thái thông báo(1 - success, 0 - Fail, 2 - null)
+	 */
+	@SuppressWarnings("deprecation")
+	public void showAlertDialog(String title, String message, int status) {
+		AlertDialog dialog = new AlertDialog.Builder(context).create();
+		dialog.setTitle(title);
+		dialog.setMessage(message);
+		if (status != 2) {
+			dialog.setIcon((status == 1) ? R.drawable.success : R.drawable.fail);
+		}
+		dialog.setButton2("OK", new OnClickListener() {
+
+			@Override
+			public void onClick(DialogInterface arg0, int arg1) {
+				// TODO Auto-generated method stub
+			}
+		});
+		dialog.show();
+	}
+	
+	@SuppressWarnings("deprecation")
+	public void adjustImageAspect(int bWidth, int bHeight, ImageView img) {
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+        if (bWidth == 0 || bHeight == 0)
+            return;
+        int sHeight = 0;
+        Activity ac = (Activity) context;
+        if (android.os.Build.VERSION.SDK_INT >= 13) {
+            Display display = ac.getWindowManager().getDefaultDisplay();
+            Point size = new Point();
+            display.getSize(size);
+            sHeight = size.y;
+        } else {
+			Display display = ac.getWindowManager().getDefaultDisplay();
+            sHeight = display.getHeight();
+        }
+        int new_width = (int) Math.floor((double) bWidth * (double) sHeight
+                / (double) bHeight);
+        params.width = new_width;
+        params.height = sHeight;
+        Log.d("", "Fullscreen image new dimensions: w = " + new_width
+                + ", h = " + sHeight);
+        img.setLayoutParams(params);
+    }
+}
